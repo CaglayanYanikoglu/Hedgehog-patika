07570730453783d77bca66a363a7a55600ffc743
CaglayanYanikoglu <caglayan@jotform.com>
2022-11-01 18:38:14 +0300

    Day 1 :: initial commit ^

---
+++ week-1/day-1/script.js
@@ -0,0 +1,28 @@
+/* window.addEventListener('DOMContentLoaded', (event) => {
+  console.log('DOM fully loaded and parsed');
+  console.log('After dom content loaded Element: ', document.querySelector('.text'));
+}); */
+
+/* console.log('script is loaded');
+console.log('Element: ', document.querySelector('.text')); */
+
+/* var name = 'caglayan';
+
+// ECMASCRIPT 6 (const and let)
+const surname = 'yanikoglu';
+
+let age = 27;
+
+age = 100;
+
+console.log('name ', name);
+console.log('surname ', surname);
+console.log('age ', age); */
+
+for(var i = 0; i < 10; i++) {
+  console.log('it is a loop');
+}
+
+console.log('i:', i);
+
+
